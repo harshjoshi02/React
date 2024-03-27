d62b71fbf6a2c0740ceae925094bc03cf41e1183
Harsh <harshjoshi162003@gmail.com>
2024-03-27 18:13:52 +0530

    add redux toolkit todo

---
+++ 11reduxToolkitTodo/README.md
@@ -0,0 +1,9 @@
+1. configure store - src/app/store.js
+
+2. create reducer - feature/todo/todoSlice.js
+
+state gives the access to the current state
+
+3. add reducer in the store
+
+4. create components then useDispatch & useSelector, useDispatch use reducer to add and change value in store
---
+++ 11reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: "hello world" }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            state.todos.map((todo) => (todo.id === action.payload ? todo.text = action.payload : todo))
+        },
+    }
+})
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
